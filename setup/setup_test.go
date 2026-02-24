db20e2cbb70a9c5ffa886c15acefc75d798c14e8
Lucas Belfanti <lhbelfanti@gmail.com>
2026-02-24 08:42:21 -0300

    ⚡️ Added setup pkg

---
+++ setup/setup_test.go
@@ -0,0 +1,35 @@
+package setup_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lhbelfanti/corpus-creator/internal/setup"
+)
+
+func TestInit_success(t *testing.T) {
+	want := "test"
+	got := setup.Init(want, nil)
+
+	assert.Equal(t, want, got)
+}
+
+func TestInit_fails(t *testing.T) {
+	assert.Panics(t, func() {
+		_ = setup.Init("test", errors.New("initialization failed"))
+	})
+}
+
+func TestMust_success(t *testing.T) {
+	assert.NotPanics(t, func() {
+		setup.Must(nil)
+	})
+}
+
+func TestMust_fails(t *testing.T) {
+	assert.Panics(t, func() {
+		setup.Must(errors.New("initialization failed"))
+	})
+}
